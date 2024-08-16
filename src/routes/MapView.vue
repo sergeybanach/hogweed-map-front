b10e13de14a1f67f33bcd04be68314070e943798
sergey <sergeybanach@gmail.com>
2024-08-17 00:11:13 +0300

    mapRelative class

--- src/routes/MapView.vue
+++ src/routes/MapView.vue
@@ -15,8 +15,23 @@ const _data = reactive({
 
 onMounted(async () => {
   gMap.map = new Map({
-    style: "https://demotiles.maplibre.org/style.json",
     container: "map",
+    style: {
+      version: 8,
+      sources: {
+        osm: {
+          type: "raster",
+          tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+        },
+      },
+      layers: [
+        {
+          id: "osm",
+          source: "osm",
+          type: "raster",
+        },
+      ],
+    },
   });
 
   gMap.map.on("click", async (e: any) => {
@@ -32,46 +47,53 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div id="mapWrapper" class="relative h-[400px]">
-    <div
-      id="map"
-      class="absolute h-full left-0 right-0 top-0 bottom-0 z-10"
-    ></div>
-    <AddingLocation
-      v-if="useUiStore().isAddingLocation"
-      :send-callback="async () => {}"
-    ></AddingLocation>
-  </div>
-
-  <Button
-    :on-click="
-      async () => {
-        useUiStore().isAddingLocation = true;
-        setMapCursor();
-      }
-    "
-    >{{
-      useUiStore().isAddingLocation ? "Adding location" : "Add location"
-    }}</Button
-  >
-  <Button
-    v-if="useUiStore().isAddingLocation === true"
-    custom-classes="bg-red-800 ml-4"
-    material-icon="cancel"
-    :on-click="
-      async () => {
-        useUiStore().isAddingLocation = false;
-        setMapCursor();
-        if (gMap.marker !== null) {
-          gMap.marker.remove();
-          gMap.marker = null;
+  <div>
+    <div id="mapRelative" class="relative h-[600px]">
+      <div
+        id="map"
+        class="absolute h-full left-0 right-0 top-0 bottom-0 z-10"
+      ></div>
+      <AddingLocation
+        v-if="useUiStore().isAddingLocation"
+        :send-callback="async () => {}"
+      ></AddingLocation>
+    </div>
+    <Button
+      class="mt-2"
+      :on-click="
+        async () => {
+          useUiStore().isAddingLocation = true;
+          // setMapCursor();
+          if (useUiStore().isAddingLocation === true) {
+            gMap.map.getCanvas().style.cursor = 'crosshair';
+          } else {
+            gMap.map.getCanvas().style.cursor = '';
+          }
         }
-      }
-    "
-  >
-    cancel
-  </Button>
-  <LocationTable :locations="_data.locations"></LocationTable>
+      "
+      >{{
+        useUiStore().isAddingLocation ? "Adding location" : "Add location"
+      }}</Button
+    >
+    <Button
+      v-if="useUiStore().isAddingLocation === true"
+      custom-classes="bg-red-800 ml-4"
+      material-icon="cancel"
+      :on-click="
+        async () => {
+          useUiStore().isAddingLocation = false;
+          setMapCursor();
+          if (gMap.marker !== null) {
+            gMap.marker.remove();
+            gMap.marker = null;
+          }
+        }
+      "
+    >
+      cancel
+    </Button>
+    <LocationTable :locations="_data.locations"></LocationTable>
+  </div>
 </template>
 
 <style scoped>
